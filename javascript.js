e0eb8c6b2d0eec003d5600e66cf2618ffef5335e
SerenityUnderwood <le.serenityunderwood@gmail.com>
2022-08-28 15:34:54 -0500

    Add pseudocode to javascript.js file

---
+++ javascript.js
@@ -0,0 +1,49 @@
+// ROCK, PAPER, SCISSORS SELCTION CHOICE
+// Create a list of choices (rock, paper, and scissors)
+
+// COMPUTER'S CHOICE OF ROCK, PAPER, SCISSORS
+//  Create function called getComputerChoice ;
+//      which will get a random value from 'list of choices'how to ;
+//      return
+
+// PLAYER'S CHOICE OF ROCK, PAPER, SCISSORS
+//  Create function called playerSelection ;
+//      that will ask the player what their choice is (case-insensitive) ;
+//      return
+
+// PLAYING A ROUND
+//  Create a function called playRound ;
+//  with two parameters (a computer's choice, a player's choice) ; 
+
+//      Create an if else statement of "if you choose rock" ;
+//          If the computer chose "Rock" ;
+//              You tied
+//          If the computer chose "Paper" ;
+//              You lose
+//          If the computer chose "Scissors" ;
+//              You won
+
+//      Create an if else statement of "if you choose paper" ;
+//          If the computer chose "Rock" ;
+//              You won
+//          If the computer chose "Paper" ;
+//              You tied
+//          If the computer chose "Scissors" ;
+//              You lose
+
+//      Create an if else statement of "if you choose scissors" ;
+//          If the computer chose "Rock" ;
+//              You lose
+//          If the computer chose "Paper" ;
+//              You won
+//          If the computer chose "Scissors" ;
+//              You tied
+
+//      Make each if else statement 'return'
+
+// FIRST TO FIVE POINTS WINS!
+// Create a function called game ;
+//      make it run five times ;
+//          for each time the player wins, they get a point ;
+//          for each time the computer wins, they get a point ;
+//      return
