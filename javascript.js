f9dcae9b3b8a91cb275691f36154da277572fce5
SerenityUnderwood <le.serenityunderwood@gmail.com>
2022-09-04 20:02:25 -0500

    Insert choice functions into JavaScript file

--- javascript.js
+++ javascript.js
@@ -1,49 +1,19 @@
-// ROCK, PAPER, SCISSORS SELCTION CHOICE
-// Create a list of choices (rock, paper, and scissors)
+let choice = [
+    "rock", 
+    "paper", 
+    "scissors"
+];
 
-// COMPUTER'S CHOICE OF ROCK, PAPER, SCISSORS
-//  Create function called getComputerChoice ;
-//      which will get a random value from 'list of choices'how to ;
-//      return
+function getComputerChoice() {
+    choice = choice[Math.floor(Math.random() * choice.length)]; // random selection
+    return(choice);
+};
 
-// PLAYER'S CHOICE OF ROCK, PAPER, SCISSORS
-//  Create function called playerSelection ;
-//      that will ask the player what their choice is (case-insensitive) ;
-//      return
+let selectedChoice = prompt("Type in rock, paper, or scissors.");
+function playerSelection() {
+    selectedChoice = selectedChoice.toLowerCase();
+    return(selectedChoice);
+};
 
-// PLAYING A ROUND
-//  Create a function called playRound ;
-//  with two parameters (a computer's choice, a player's choice) ; 
-
-//      Create an if else statement of "if you choose rock" ;
-//          If the computer chose "Rock" ;
-//              You tied
-//          If the computer chose "Paper" ;
-//              You lose
-//          If the computer chose "Scissors" ;
-//              You won
-
-//      Create an if else statement of "if you choose paper" ;
-//          If the computer chose "Rock" ;
-//              You won
-//          If the computer chose "Paper" ;
-//              You tied
-//          If the computer chose "Scissors" ;
-//              You lose
-
-//      Create an if else statement of "if you choose scissors" ;
-//          If the computer chose "Rock" ;
-//              You lose
-//          If the computer chose "Paper" ;
-//              You won
-//          If the computer chose "Scissors" ;
-//              You tied
-
-//      Make each if else statement 'return'
-
-// FIRST TO FIVE POINTS WINS!
-// Create a function called game ;
-//      make it run five times ;
-//          for each time the player wins, they get a point ;
-//          for each time the computer wins, they get a point ;
-//      return
+console.log(getComputerChoice());
+console.log(playerSelection());
